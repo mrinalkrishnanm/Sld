71f9c5e4a2fe93b1b14eb10f8e2f6ae546f67835
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-04-22 17:57:10 +0530

    started basic maths

--- src/basic-maths/BasicMathQuesList.js
+++ src/basic-maths/BasicMathQuesList.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 class BasicMathQuesList extends React.Component{
-	
+	render(){
+		return(<h1>hi</h1>)
+	}
 }
+
+module.exports = BasicMathQuesList;
--- src/basic-maths/ChildQues6.js
+++ src/basic-maths/ChildQues6.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
+import BasicMathQuesList from './BasicMathQuesList';
+
 class ChildQues6 extends React.Component{
     constructor(){
 		super();
@@ -14,7 +16,7 @@ class ChildQues6 extends React.Component{
         var questions=[{id:1,question:"Multiply 38 by 10",answers:[300,0.38,3.8,380]},
         {id:2,question:"Add 3 and 8",answers:[5,11,9,14]},
         {id:3,question:"Which is bigger 7 or 5?",answers:[7,5,"Not sure"]},
-        {id:4,question:"What does > symbol mean?"} answers:["greater than","turn right","next page","not sure"]}
+        {id:4,question:"What does > symbol mean?",answers:["greater than","turn right","next page","not sure"]}
         ]
 		return(
 			<div>
@@ -29,7 +31,7 @@ class ChildQues6 extends React.Component{
                     <p>please enter the rules that are to be followed by the parent. This space here tells the parent the about this exercise and the points that are to be noted</p>
                     <button onClick={this.closeModal.bind(this)}>close</button>
             	</Modal>
-                <BasicMathQuesList questions={questions}>
+                <BasicMathQuesList questions={questions}/>
 			</div>
 			)
 	}

cc7c93f842198165f9f72bacb142dd4675f533b3
Akhil Ramesh <akkhil@outlook.com>
2017-04-27 12:00:01 +0530

    added result fetch

--- src/result/Result.js
+++ src/result/Result.js
@@ -5,47 +5,52 @@ import _ from 'lodash';
 
 class Result extends React.Component{
 
-	componentWillMount(){
-		this.getResult();
-	}
-	
-	getResult(){
-		var retrievedObject = localStorage.getItem('attributes');
-        var answers = JSON.parse(retrievedObject);
-        for (var key in answers) {
-            //console.log(key)
-            if (answers[key]>50) {
-                    answers[key] = 1
-          
-            }
-            else{
-            	 answers[key] = 0
-            }
-            	
-         }
+  componentWillMount(){
+    this.getResult();
+  }
 
-         var array= Object.keys(answers).map(function(key){return answers[key];});
-	// 	var value=[1,0,0,0,0,1,0,1,0,1,1,0,1,1,1,0];
-	// 	var data={
-	// 		value:value
-	// 	}
-	// 	var success = (res) => {
- //        	console.log("Got Result");
- //        	console.log(res);
- //    	}
- //    	var failure = (res) => {
- //      		console.log("Can't load Result");
- //    	}
-	// 	API.post("http://localhost:3000/users/foo",data,success,failure);
-		console.log(array);
-	}
-	render(){
-		return(
-			<div>
-				<h1>helo</h1>
-			</div>
-			);
-	}
+  getResult(){
+    var retrievedObject = localStorage.getItem('attributes');
+    var answers = JSON.parse(retrievedObject);
+    for (var key in answers) {
+      //console.log(key)
+      if (answers[key]>50) {
+        answers[key] = 1
+
+      }
+      else{
+        answers[key] = 0
+      }
+
+    }
+
+    var array= Object.keys(answers).map(function(key){return answers[key];});
+    Request.post("http://localhost:3000/users/foo")
+    .send({value: array})
+    .end((err,res) => {
+      console.log(res)
+    })
+    // 	var value=[1,0,0,0,0,1,0,1,0,1,1,0,1,1,1,0];
+    // 	var data={
+    // 		value:value
+    // 	}
+    // 	var success = (res) => {
+    //        	console.log("Got Result");
+    //        	console.log(res);
+    //    	}
+    //    	var failure = (res) => {
+    //      		console.log("Can't load Result");
+    //    	}
+    // 	API.post("http://localhost:3000/users/foo",data,success,failure);
+    console.log(array);
+  }
+  render(){
+    return(
+      <div>
+      <h1>helo</h1>
+      </div>
+    );
+  }
 }
 
 module.exports = Result;
